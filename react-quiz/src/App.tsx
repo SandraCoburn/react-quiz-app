41c07a7ed7c0f62888182655ae0ce2c6adeec8fd
Sandra Coburn <sandra_coburn@hotmail.com>
2020-10-30 14:48:58 -0700

    Added import types to questionCard

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -6,7 +6,7 @@ import QuestionCard from "./components/QuestionCard";
 //types
 import { QuestionState, Difficulty } from "./API";
 
-type AnswerObject = {
+export type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
@@ -77,7 +77,7 @@ const App = () => {
         </button>
       ) : null}
 
-      {!gameOver ? <p className="score">Score:</p> : null}
+      {!gameOver ? <p className="score">Score:{score}</p> : null}
       {loading && <p>Loading Questions ...</p>}
       {!loading && !gameOver && (
         <QuestionCard
